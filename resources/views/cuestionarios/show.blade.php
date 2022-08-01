0f2f87e0d53b6a8b074c78a2efe80b4c44dacd3f
clodd18 <clodd18@gmail.com>
2022-08-01 03:04:43 +0200

    modificacion cuestionarios

--- resources/views/cuestionarios/show.blade.php
+++ resources/views/cuestionarios/show.blade.php
@@ -36,7 +36,7 @@
                 </div>
                 <div class="flex items-center justify-center mt-4">
                 <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
-                    Enviar formulario</button>
+                    Enviar cuestionario</button>
                 </div>
             </form>
         </div>
