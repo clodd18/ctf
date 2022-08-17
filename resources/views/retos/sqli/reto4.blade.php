c6933f528b132af18e9fac711bc01917d7a73431
clodd18 <clodd18@gmail.com>
2022-08-17 17:16:34 +0200

    sqli

--- resources/views/retos/sqli/reto4.blade.php
+++ resources/views/retos/sqli/reto4.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Inyección de SQL ciega basada en tiempo') }}
+            {{ __('Inyección de SQL ciega con token') }}
         </h2>
     </x-slot>
 
